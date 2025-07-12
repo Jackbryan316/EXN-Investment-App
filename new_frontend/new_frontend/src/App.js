12ff1c66712ed6a9531bb30bd04d852c1b7c552f
Jackbryan316 <Jackbryan316@gmail.com>
2025-07-12 12:48:04 +0100

    Add files via upload

---
+++ new_frontend/new_frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to EXN Investment Platform</h1>
+      <p>Frontend is Live and Connected ✅</p>
+    </div>
+  );
+}
+
+export default App;
